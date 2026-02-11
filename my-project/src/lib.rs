86f688c462ed59bdaae4e11fcdd7caded253dd9b
Zhao Guangjian <zgj0315@gmail.com>
2026-02-11 17:07:32 +0800

    android apk output

---
+++ my-project/src/lib.rs
@@ -0,0 +1,12 @@
+#[cfg(target_os = "android")]
+#[unsafe(no_mangle)]
+fn android_main(app: slint::android::AndroidApp) {
+    slint::android::init(app).unwrap();
+
+    slint::slint! {
+        export component MainWindow inherits Window {
+            Text { text: "Hello World"; }
+        }
+    }
+    MainWindow::new().unwrap().run().unwrap();
+}
