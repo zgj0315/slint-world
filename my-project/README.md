c90f37ea68a85fa9c19988db1d2f7fee8696acb9
Zhao Guangjian <zgj0315@gmail.com>
2026-02-11 17:32:35 +0800

    works

--- my-project/README.md
+++ my-project/README.md
@@ -10,6 +10,13 @@ cargo packager -r
 
 ## Android Apk
 ```
+# list device
+x devices
+
+# run on device
+x run --device adb:30906b3e
+
+# package apk
 x build --platform android --arch arm64 --format apk --release
 
 ```
--- my-project/src/lib.rs
+++ my-project/src/lib.rs
@@ -1,12 +1,27 @@
+use std::error::Error;
+
+slint::include_modules!();
+
+pub fn run() -> Result<(), Box<dyn Error>> {
+    let ui = AppWindow::new()?;
+
+    // 复用之前的计数器逻辑
+    ui.on_request_increase_value({
+        let ui_handle = ui.as_weak();
+        move || {
+            let ui = ui_handle.unwrap();
+            ui.set_counter(ui.get_counter() + 1);
+        }
+    });
+
+    ui.run()?;
+    Ok(())
+}
+
 #[cfg(target_os = "android")]
 #[unsafe(no_mangle)]
 fn android_main(app: slint::android::AndroidApp) {
     slint::android::init(app).unwrap();
 
-    slint::slint! {
-        export component MainWindow inherits Window {
-            Text { text: "Hello World"; }
-        }
-    }
-    MainWindow::new().unwrap().run().unwrap();
+    run().unwrap();
 }
